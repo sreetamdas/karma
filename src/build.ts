e7b77dd77d6f7712c65945acfa7ff5982d33b462
Sreetam Das <sreetamdas@gmail.com>
2023-07-31 11:54:48 +0530

    split into build file and index export

---
+++ src/build.ts
@@ -0,0 +1,22 @@
+import { promises as fs } from "fs";
+import { generateTheme } from "./generateTheme";
+
+const DIR = "./themes";
+
+const defaultTheme = generateTheme();
+const lightTheme = generateTheme("light");
+
+fs.mkdir(DIR, { recursive: true })
+	.then(() =>
+		Promise.all([
+			fs.writeFile(
+				`${DIR}/default.json`,
+				JSON.stringify(defaultTheme, null, 2),
+			),
+			fs.writeFile(`${DIR}/light.json`, JSON.stringify(lightTheme, null, 2)),
+		]),
+	)
+	.then(() => {
+		console.log("Theme generated");
+	})
+	.catch(() => process.exit(1));
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
-import { promises as fs } from "fs";
 import { generateTheme } from "./generateTheme";
-
-const DIR = "./themes";
 
 const defaultTheme = generateTheme();
 const lightTheme = generateTheme("light");
-
-fs.mkdir(DIR, { recursive: true })
-	.then(() =>
-		Promise.all([
-			fs.writeFile(
-				`${DIR}/default.json`,
-				JSON.stringify(defaultTheme, null, 2),
-			),
-			fs.writeFile(`${DIR}/light.json`, JSON.stringify(lightTheme, null, 2)),
-		]),
-	)
-	.then(() => {
-		console.log("Theme generated");
-	})
-	.catch(() => process.exit(1));
 
 export { defaultTheme, lightTheme };
